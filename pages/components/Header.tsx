cca54cd513ad89b5f9c630cb696c7f3a2a60e3b4
sehaku <none@DESKTOP-1C7AGIU.localdomain>
2021-10-25 16:37:11 +0900

    [Change] Apply useColorModeValue

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,10 +1,12 @@
-import type { NextPage } from 'next'
-import { Heading } from '@chakra-ui/react'
-
+import type { NextPage } from "next";
+import { Heading } from "@chakra-ui/react";
+import { useColorModeValue } from "@chakra-ui/color-mode";
 const Header: NextPage = () => {
   return (
-      <Heading as={'h6'} color="red.400">Hello, nextjs</Heading>
-  )
-}
+    <Heading as={"h6"} color={useColorModeValue("red.400", "green")}>
+      Hello, nextjs
+    </Heading>
+  );
+};
 
-export default Header
+export default Header;
