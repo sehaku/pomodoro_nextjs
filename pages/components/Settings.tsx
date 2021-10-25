67afe9873d00c1159971d68158e69f8dfb790a3c
sehaku <none@DESKTOP-1C7AGIU.localdomain>
2021-10-26 02:32:12 +0900

    [Add] Add ModalView and props(isSettingChange)

--- pages/components/Settings.tsx
+++ pages/components/Settings.tsx
@@ -1,15 +1,64 @@
-import type { NextPage } from "next";
 import { SettingsIcon } from "@chakra-ui/icons";
-import { IconButton } from "@chakra-ui/react";
+import {
+  IconButton,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { Dispatch, MutableRefObject, SetStateAction } from "react";
 
-const Settings: NextPage = () => {
+type Props = {
+  isSettingChange: boolean;
+  setIsSettingChange: Dispatch<SetStateAction<boolean>>;
+};
+
+const Settings = (props: Props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <IconButton
         aria-label="Search database"
         _focus={{ boxShadow: "none" }}
-        icon={<SettingsIcon/>}
+        icon={<SettingsIcon />}
+        isRound={true}
+        onClick={onOpen}
       />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud
+            ullamco deserunt aute id consequat veniam incididunt duis in sint
+            irure nisi. Mollit officia cillum Lorem ullamco minim nostrud elit
+            officia tempor esse quis. Sunt ad dolore quis aute consequat. Magna
+            exercitation reprehenderit magna aute tempor cupidatat consequat
+            elit dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt
+            cillum quis. Velit duis sit officia eiusmod Lorem aliqua enim
+            laboris do dolor eiusmod. Et mollit incididunt nisi consectetur esse
+            laborum eiusmod pariatur proident Lorem eiusmod et. Culpa deserunt
+            nostrud ad veniam.
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button colorScheme="yellow" mr={3} onClick={() => {
+              props.setIsSettingChange(true);
+            }}>
+              Apply
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
