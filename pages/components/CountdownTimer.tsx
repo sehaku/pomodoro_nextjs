bdf0f46e2abf52d7ad921c6ca486067d998755cc
sehaku <none@DESKTOP-1C7AGIU.localdomain>
2021-11-01 09:22:11 +0900

    [Add] Use react-countdown for timer.

---
+++ pages/components/CountdownTimer.tsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { Text } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import Countdown, {
+  CountdownApi,
+  CountdownRenderProps,
+  zeroPad,
+} from "react-countdown";
+type Props = {
+  duration: number;
+  handleEnd: () => void;
+  handleSetIsPlay: (v: boolean) => void;
+  isPlay: boolean;
+  isPomodoro: boolean;
+};
+export default class CountdownTimer extends Component<Props, {}> {
+  countdownApi: CountdownApi | null = null;
+  state = { date: Date.now() + 10000 };
+  handleStartClick = (): void => {
+    console.log("start");
+    this.countdownApi && this.countdownApi.start();
+    this.props.handleSetIsPlay(true);
+  };
+
+  handlePauseClick = (): void => {
+    console.log("pause");
+    this.countdownApi && this.countdownApi.pause();
+    this.props.handleSetIsPlay(false);
+  };
+
+  handleResetClick = (): void => {
+    console.log("reset");
+    this.setState({ date: Date.now() + this.props.duration });
+    this.props.handleSetIsPlay(true);
+  };
+
+  setRef = (countdown: Countdown | null): void => {
+    if (countdown) {
+      this.countdownApi = countdown.getApi();
+    }
+  };
+
+  handleEnd = (): void => {
+    this.props.handleEnd();
+    this.setState({ date: Date.now() + this.props.duration });
+  };
+  renderer = (render: CountdownRenderProps) => {
+    return (
+      <Text fontSize="9xl">
+        {zeroPad(render.minutes, 2)}:{zeroPad(render.seconds, 2)}
+      </Text>
+    );
+  };
+  render() {
+    return (
+      <Flex alignItems="center" direction="column">
+        <Countdown
+          key={this.state.date}
+          ref={this.setRef}
+          date={this.state.date}
+          autoStart={true}
+          onComplete={this.handleEnd}
+          renderer={this.renderer}
+        />
+        <ButtonGroup spacing="3">
+          <Button
+            display={this.props.isPlay ? "none" : ""}
+            onClick={this.handleStartClick}
+          >
+            Start
+          </Button>
+          <Button
+            display={this.props.isPlay ? "" : "none"}
+            onClick={this.handlePauseClick}
+          >
+            Pause
+          </Button>
+          <Button onClick={this.handleResetClick}>Reset</Button>
+        </ButtonGroup>
+      </Flex>
+    );
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
 import { Heading, IconButton, useColorMode, Flex } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import Header from "./components/Header";
 import Settings from "./components/Settings";
 import Timer from "./components/Timer";
+
 const Home: NextPage = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
